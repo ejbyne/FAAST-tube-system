28fa3db2c98121ad4e741000b8e3f375570cc3a1
ed <ejbyne@gmail.com>
2014-11-01 23:18:27 +0000

    Initial version of station class completed

--- lib/station.rb
+++ lib/station.rb
@@ -26,20 +26,32 @@ class Station
     @passengers.count
   end
 
-  def receive_train(train)
-    @trains << train
+  def full_of_passengers?
+    passenger_count == passenger_capacity
   end
 
-  def receive_passenger(passenger)
-    @passengers << passenger
+  def full_of_trains?
+    train_count == train_capacity
   end
 
-  def release_train(train)
-    @trains.delete(train)
+  def receive_train(*train)
+    #raise "Cannot process request" unless train.first.is_a?(Train)
+    raise "Station full" if full_of_trains?
+    @trains << train.first
   end
 
-  def release_passenger(passenger)
-    @passengers.delete(passenger)
+  def receive_passenger(*passenger)
+    #raise "Cannot process request" unless passenger.first.is_a?(Passenger)
+    raise "Station full" if full_of_passengers?
+    @passengers << passenger.first
+  end
+
+  def release_train(*train)
+    @trains.delete(train.first)
+  end
+
+  def release_passenger(*passenger)
+    @passengers.delete(passenger.first)
   end
 
 
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -6,6 +6,14 @@ describe Station do
   let (:train) { double :train }
   let (:passenger) { double :passenger }
 
+  def fill_station_with_passengers
+    300.times { station.receive_passenger(passenger) }
+  end
+
+  def fill_station_with_trains
+    6.times { station.receive_train(train) }
+  end
+
   it "should have default capacities if none are specified" do
     default_station = Station.new
     expect(default_station.train_capacity).to eq(2)
@@ -37,19 +45,81 @@ describe Station do
 
   it "should be able to release a passenger" do
     station.receive_passenger(passenger)
-    station.release_passenger (passenger)
+    station.release_passenger(passenger)
     expect(station.passenger_count).to eq(0)
   end
 
-  it "should know when it's full"
+  it "should know when it's full of passengers" do
+    expect(station).not_to be_full_of_passengers
+    fill_station_with_passengers
+    expect(station).to be_full_of_passengers
+  end
 
-  it "cannot receive more passengers than its capacity"
+  it "should know when it's full of trains" do
+    expect(station).not_to be_full_of_trains
+    fill_station_with_trains
+    expect(station).to be_full_of_trains
+  end
 
-  it "cannot receive more than trains than its capacity"
+  it "cannot receive more passengers than its capacity" do
+    fill_station_with_passengers
+    expect(lambda { station.receive_passenger(passenger) }).to raise_error(RuntimeError)
+  end
 
-  it "should only receive trains of the correct class"
+  it "cannot receive more than trains than its capacity" do
+    fill_station_with_trains
+    expect(lambda { station.receive_train(train)} ).to raise_error(RuntimeError)
+  end
 
-  it "should only accept one passenger or train at a time"
+  # it "should only receive tube trains with receive_train" do
+  #   expect(lambda { station.receive_train(:not_a_train) }).to raise_error(RuntimeError)
+  # end
+
+  # it "should only receive passengers with receive_passenger" do
+  #   expect(lambda { station.receive_passenger(:not_a_passenger)} ).to raise_error(RuntimeError)
+  # end
+
+  # it "should raise an error if empty argument is passed to receive_passenger" do
+  #   expect(lambda { station.receive_passenger() } ).to raise_error(RuntimeError)
+  # end
+
+  # it "should raise an error if empty argument is passed to receive_train" do
+  #   expect(lambda {station.receive_train()} ).to raise_error(RuntimeError)
+  # end
+
+  it "should only accept one passenger at a time" do
+    station.receive_passenger(passenger, passenger)
+    expect(station.passenger_count).to eq(1)
+  end
+
+  it "should only accept one train at at time" do
+    station.receive_train(train, train)
+    expect(station.train_count).to eq(1)
+  end
+
+  it "should only release one passenger at a time" do
+    second_passenger = double :passenger
+    station.receive_passenger(passenger)
+    station.receive_passenger(second_passenger)
+    station.release_passenger(passenger, second_passenger)
+    expect(station.passenger_count).to eq(1)
+  end
+
+  it "should only release one train at a time" do
+    second_train = double :train
+    station.receive_train(train)
+    station.receive_train(second_train)
+    station.release_train(train, second_train)
+    expect(station.train_count).to eq(1)
+  end
+
+  # it "should not release a passenger who isn't there" do
+  #   expect(lambda {station.release_passenger(passenger)} ).to raise_error(RuntimeError)
+  # end
+
+  # it "should not release a train which isn't there" do
+  #   expect(lambda {station.release_train(train)} ).to raise_error(RuntimeError)
+  # end
 
   it "should only receive passengers with an account and a minimum credit of £2"
 
