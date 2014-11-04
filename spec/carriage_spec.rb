8334abd9d519e633ed9c3840963cdf659d6e7932
ed <ejbyne@gmail.com>
2014-11-04 14:10:02 +0000

    Updated Carriage tests to include doubles

--- spec/carriage_spec.rb
+++ spec/carriage_spec.rb
@@ -3,10 +3,22 @@ require 'carriage'
 describe Carriage do
 
   it "should have a passenger capacity of 40" do
-    station = Station.new
-    train = Train.new(initial_station: station)
+    station = double :station
+    train = double :train
     carriage = Carriage.new(train: train)
     expect(carriage.passenger_capacity).to eq(40)
   end
 
 end
+
+# TESTS WITHOUT DOUBLES:
+# describe Carriage do
+
+#   it "should have a passenger capacity of 40" do
+#     station = Station.new
+#     train = Train.new(initial_station: station)
+#     carriage = Carriage.new(train: train)
+#     expect(carriage.passenger_capacity).to eq(40)
+#   end
+
+# end
